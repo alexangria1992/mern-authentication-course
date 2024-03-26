6e695a309aee5b3fe637994bedbc49f3eff8049b
Alexandre Angrignon <alex040892@gmail.com>
2024-03-26 18:06:44 +1300

    env file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
-const port = 5000;
+import dotenv from "dotenv";
+import colors from "colors";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Server is ready"));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () =>
+  console.log(colors.cyan(`Server started on port ${port}`))
+);
