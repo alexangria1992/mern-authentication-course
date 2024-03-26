996d0de55673a17166dac31c553f78df2198e67e
Alexandre Angrignon <alex040892@gmail.com>
2024-03-26 19:31:27 +1300

    User Controller Functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,48 @@
 import asyncHandler from "express-async-handler";
 
-// @esc Auth user/set token
+// @desc Auth user/set token
 // route POST / api/users/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
+  // res.status(401);
+  // throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc Register a new user
+// route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "register User" });
+});
+
+// @desc Register a new user
+// route POST /api/users/logout
+// @access Public
+const logoutUSer = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc Get user profile
+// route GET /api/users/profile
+// @access Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+
+// @desc Update user profile
+// route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUSer,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,9 @@ const app = express();
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(colors.cyan(`Server started on port ${port}`))
