c9e673613eb5a46ad3fe3e342ae963087580776d
Alexandre Angrignon <alex040892@gmail.com>
2024-03-29 13:52:15 +1300

    Auth User Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,20 @@ import User from "../models/userModel.js";
 const authUser = asyncHandler(async (req, res) => {
   // res.status(401);
   // throw new Error("Something went wrong");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Inavlid email or password");
+  }
   res.status(200).json({ message: "Auth User" });
 });
 
