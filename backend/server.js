42f12b1eb6b83d20237b1ef3b0cf5a57d03df5b0
Alexandre Angrignon <alex040892@gmail.com>
2024-03-29 15:11:17 +1300

    Auth Protect Middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 connectDB();
@@ -13,6 +15,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
